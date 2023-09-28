f5d39b6d2e747808919840b9f4ad8be35cc0a527
Anna Laws <a.laws2@exeter.ac.uk>
2023-09-28 12:01:30 +0100

    :broom: Refactoring

--- pages/2_Interactive_demo.py
+++ pages/2_Interactive_demo.py
@@ -52,28 +52,30 @@ except FileNotFoundError:
 # import utilities.container_details
 
 
-
 def build_lists_for_each_team_and_year(
         stroke_team_list,
         year_options,
         region_team_list
         ):
+    """
+    Builds a list containing:
+    Team 1 (2016 to 2021)
+    Team 1 (2016)
+    Team 1 (2017)
+    ...
+    Team 1 (2021)
+    Team 2 (2016 to 2021)
+    Team 2 (2016)
+    Team 2 (2017)
+    ...
+    Team 2 (2021)
+    """
     stroke_team_list_years = [
         f'{s} ({y})'
         for s in ['all E+W'] + stroke_team_list
         for y in year_options
     ]
-    # Builds a list containing:
-    # Team 1 (2016 to 2021)
-    # Team 1 (2016)
-    # Team 1 (2017)
-    # ...
-    # Team 1 (2021)
-    # Team 2 (2016 to 2021)
-    # Team 2 (2016)
-    # Team 2 (2017)
-    # ...
-    # Team 2 (2021)
+
     # Similar for regions:
     region_team_list_years = [
         region
@@ -83,42 +85,26 @@ def build_lists_for_each_team_and_year(
     return stroke_team_list_years, region_team_list_years
 
 
-def inputs_region_choice(region_list):
-    cols_regions = st.columns(3)
+def inputs_region_choice(df_stroke_team, containers=[]):
+    # List of regions
+    region_list = sorted(list(set(
+        df_stroke_team['RGN11NM'].squeeze().values
+        )))
+
     regions_selected_bool = []
     for r, region in enumerate(region_list):
-        with cols_regions[r % len(cols_regions)]:
+        with containers[r % len(containers)]:
             region_bool = st.checkbox(region, key=region)
         regions_selected_bool.append(region_bool)
     regions_selected = list(np.array(region_list)[regions_selected_bool])
     return regions_selected
 
 
-def input_stroke_teams_to_highlight_long(
-        stroke_team_list_years_by_region_selected,
-        all_years_str
-        ):
-    """
-    One long list of all of the options for this region
-    """
-    # Use the format function in the multiselect to change how
-    # the team names are displayed without changing their names
-    # in the underlying data.
-    # Because the current ordering of the list is by team and then
-    # by year, this also visually breaks up the list into groups of
-    # each team.
-    stroke_teams_selected = st.multiselect(
-        'Stroke team',
-        options=stroke_team_list_years_by_region_selected,
-        default=f'all E+W ({all_years_str})'
-        )
-    return stroke_teams_selected
-
-
 def input_stroke_teams_to_highlight(
-        stroke_team_list,
+        df_stroke_team,
+        regions_selected,
+        all_teams_str,
         year_options,
-        region_team_list,
         all_years_str='',
         containers=[]
         ):
@@ -128,12 +114,17 @@ def input_stroke_teams_to_highlight(
     if len(containers) < 1:
         containers = st.columns(4)
 
+    stroke_team_list = (
+        reduce_big_lists_to_regions_selected(
+            df_stroke_team,
+            regions_selected,
+            ))
+    # Add on the "all teams" team:
+    stroke_team_list = [all_teams_str] + list(
+        stroke_team_list)
+
     all_stroke_teams_selected = []
     for y, year in enumerate(year_options):
-        stroke_team_list_years, region_team_list_years = (
-            build_lists_for_each_team_and_year(
-                stroke_team_list, [year], region_team_list)
-        )
         # Use the format function in the multiselect to change how
         # the team names are displayed without changing their names
         # in the underlying data.
@@ -143,47 +134,48 @@ def input_stroke_teams_to_highlight(
         with containers[y % len(containers)]:
             stroke_teams_selected = st.multiselect(
                 f'{year}',
-                options=stroke_team_list_years,
-                default=f'all E+W ({year})' if year == all_years_str else None,
+                options=stroke_team_list,
+                default='all E+W' if year == all_years_str else None,
                 key=f'input_teams_{year}'
                 )
+        stroke_teams_selected = [
+            f'{t} ({year})' for t in stroke_teams_selected]
         all_stroke_teams_selected += stroke_teams_selected
 
     return all_stroke_teams_selected
 
 
 def reduce_big_lists_to_regions_selected(
-        region_team_list_years,
+        df_stroke_team,
         regions_selected,
-        stroke_team_list_years
-):
+    ):
+
+    # Create a shorter list of teams.
+    # This contains only teams in the selected region(s).
+    # List of stroke teams
+    stroke_team_list = list(df_stroke_team['Stroke Team'].squeeze().values)
+    # List of regions
+    region_team_list = list(df_stroke_team['RGN11NM'].squeeze().values)
+
     full_list_regions_selected_bool = np.full(
-        len(region_team_list_years), False)
+        len(region_team_list), False)
     for region in ['all E+W'] + regions_selected:
-        inds = np.where(np.array(region_team_list_years) == region)
+        inds = np.where(np.array(region_team_list) == region)
         full_list_regions_selected_bool[inds] = True
 
     stroke_team_list_years_by_region_selected = (
-        np.array(stroke_team_list_years)[full_list_regions_selected_bool]
+        np.array(stroke_team_list)[full_list_regions_selected_bool]
     )
     return stroke_team_list_years_by_region_selected
 
 
-def plot_geography_pins(region_list, df_stroke_team):
-    fig = go.Figure()
-
-    fig.update_layout(
-        width=500,
-        height=500,
-        margin_l=0, margin_r=0, margin_t=0, margin_b=0
-        )
-
-    # for region in region_list:
+def plot_geography_pins(df_stroke_team):
+    # Import geojson data:
     geojson_file = 'regions_EW.geojson'
     with open(dir + './data_descriptive/region_geojson/' + geojson_file) as f:
         geojson_ew = geojson.load(f)
-    # Find extent of this geojson data.
 
+    # Find extent of this geojson data.
     coords = np.array(list(geojson.utils.coords(geojson_ew)))
     extent = [
         coords[:, 0].min(),
@@ -192,20 +184,35 @@ def plot_geography_pins(region_list, df_stroke_team):
         coords[:, 1].max()
     ]
 
+    # Get the names of the regions out of the geojson:
+    region_list = []
+    for feature in geojson_ew['features']:
+        region = feature['properties']['RGN11NM']
+        region_list.append(region)
+
+    # Create a dummy dataframe of region names and some value for colours.
     df_regions = pd.DataFrame(region_list, columns=['RGN11NM'])
     df_regions['v'] = [0] * len(df_regions)  # Same value, same colour.
 
+    # Plot:
+    fig = go.Figure()
+    fig.update_layout(
+        width=500,
+        height=500,
+        margin_l=0, margin_r=0, margin_t=0, margin_b=0
+        )
+
+    # Add region polygons:
     fig.add_trace(go.Choropleth(
         geojson=geojson_ew,
         locations=df_regions['RGN11NM'],
         z=df_regions['v'],
         featureidkey='properties.RGN11NM',
-        # coloraxis="coloraxis",
         colorscale='Picnic',
-        # autocolorscale=False
         showscale=False,
         hoverinfo='skip'
     ))
+    # Add scatter markers for all hospitals:
     fig.add_trace(go.Scattergeo(
         lon=df_stroke_team['long'],
         lat=df_stroke_team['lat'],
@@ -213,8 +220,10 @@ def plot_geography_pins(region_list, df_stroke_team):
         mode='markers',
         # marker_color=df_stroke_team['RGN11NM']
     ))
+
+    # Update geojson projection.
     # Projection options:
-    # august  eckert1  fahey  times  van der grinten
+    #   august  eckert1  fahey  times  van der grinten
     fig.update_layout(
         geo_scope='europe',
         geo_projection=go.layout.geo.Projection(type='times'),
@@ -223,11 +232,13 @@ def plot_geography_pins(region_list, df_stroke_team):
         # geo_resolution=50,
         geo_visible=False
     )
+    fig.update_geos(fitbounds="locations", visible=False)
+
+    # Update hover info for scatter points:
     fig.update_traces(
         hovertemplate='%{customdata[0]}<extra></extra>',
         selector=dict(type='scattergeo')
     )
-    fig.update_geos(fitbounds="locations", visible=False)
 
     plotly_config = {
         'modeBarButtonsToRemove': ['lasso2d', 'select2d'],
@@ -319,6 +330,61 @@ def plot_violins(
     st.plotly_chart(fig, config=plotly_config)
 
 
+def check_teams_in_stats_df(
+        summary_stats_df,
+        stroke_teams_selected,
+        container_warnings
+        ):
+    """
+    Check that all of the requested data exists.
+    Remove any that aren't ok
+    """
+    try:
+        # Smaller dataframe of only selected teams:
+        df_to_show = summary_stats_df[stroke_teams_selected]
+    except KeyError:
+        # Remove teams that aren't in the dataframe.
+        # Keep the valid ones in here:
+        reduced_teams_to_show = []
+        # Keep the invalid ones in here:
+        missing_teams = []
+        # Set up for printing a nice warning message:
+        show_warning = False
+        warning_str = 'There is no data for '
+
+        # Sort the teams into the valid and invalid lists:
+        for team in stroke_teams_selected:
+            try:
+                summary_stats_df[team]
+                reduced_teams_to_show.append(team)
+            except KeyError:
+                show_warning = True
+                missing_teams.append(team)
+
+        # The dataframe containing only the valid teams:
+        df_to_show = summary_stats_df[reduced_teams_to_show]
+
+        # If there were missing teams (should always be True),
+        # show the message.
+        if show_warning is True:
+            # Create a warning message to print.
+            # e.g. "There is no data for {1}, {2} or {3}."
+            warning_str = (
+                warning_str +
+                ', '.join(missing_teams[:-1])
+                )
+            if len(missing_teams) > 1:
+                warning_str += ' or '
+            warning_str = (
+                warning_str + missing_teams[-1] + '.'
+            )
+            # Display the warning:
+            with container_warnings:
+                st.warning(warning_str, icon='⚠️')
+
+    return df_to_show
+
+
 def main():
     # ###########################
     # ##### START OF SCRIPT #####
@@ -328,79 +394,40 @@ def main():
     # Title:
     st.markdown('# 📊 Descriptive statistics')
 
-    # ###########################
-    # ########## SETUP ##########
-    # ###########################
+    # Build up the page layout:
+    _ = """
+    +-----------------------------------------------------------------+
+    | 📊 Descriptive statistics                                       |
+    | <---------cols_inputs_map[0]---------><---cols_inputs_map[1]--->|
+    |                                      |                          |
+    |       container_input_regions        |                          |
+    |                                      |      container_map       |
+    |        container_input_teams         |                          |
+    +--------------------------------------+--------------------------+
+    |                   container_input_4hr_toggle                    |
+    +-----------------------------------------------------------------+
+    |                        container_results                        |
+    +-----------------------------------------------------------------+
+    |                        container_violins                        |
+    +-----------------------------------------------------------------+
+    """
 
-    all_regions_str = 'all E+W'
-    all_teams_str = 'all E+W'
+    cols_inputs_map = st.columns([0.6, 0.4])
+    with cols_inputs_map[0]:
+        container_input_regions = st.container()
+    with cols_inputs_map[0]:
+        container_input_teams = st.container()
+    with cols_inputs_map[1]:
+        container_map = st.container()
 
-    # Import list of all stroke teams:
-    df_stroke_team = pd.read_csv(
-        dir + './data_descriptive/hospitals_and_lsoas_descriptive_stats.csv',
-        index_col=False).sort_values('Stroke Team')
+    container_warnings = st.container()
+    container_input_4hr_toggle = st.container()
+    container_results_table = st.container()
+    container_violins = st.container()
 
-    # List of stroke teams
-    stroke_team_list = list(df_stroke_team['Stroke Team'].squeeze().values)
-
-    # List of regions
-    region_list = sorted(list(set(df_stroke_team['RGN11NM'].squeeze().values)))
-    region_team_list = list(df_stroke_team['RGN11NM'].squeeze().values)
-
-    region_list = [
-        'North East',
-        'North West',
-        'Yorkshire and The Humber',
-        'East Midlands',
-        'West Midlands',
-        'Wales',
-        'South West',
-        'South East',
-        'East of England',
-        'London',
-    ]
-    
-    region_set = [all_regions_str] + region_list
-
-    cols_regions = st.columns([0.6, 0.4])
-    with cols_regions[1]:
-        # Plot the team locations
-        plot_geography_pins(region_list, df_stroke_team)
-
-    # Add in all the year options:
-    all_years_str = '2016 to 2021'
-    year_options = [all_years_str, '2016', '2017',
-                    '2018', '2019', '2020', '2021']
-    # ^ to do - change this to read it from new "years" index
-
-    stroke_team_list_years, region_team_list_years = (
-        build_lists_for_each_team_and_year(
-            stroke_team_list, year_options, region_team_list)
-    )
-
-    with cols_regions[0]:
-        # Select a region:
-        st.markdown('Show teams from these regions:')
-        regions_selected = inputs_region_choice(region_list)
-
-    # Reduce the big lists to only regions selected:
-    (stroke_team_list_years_by_region_selected
-     ) = reduce_big_lists_to_regions_selected(
-        region_team_list_years,
-        regions_selected,
-        stroke_team_list_years
-        )
-
-    (stroke_team_list_by_region_selected
-     ) = reduce_big_lists_to_regions_selected(
-        region_team_list,
-        regions_selected,
-        stroke_team_list
-        )
-    stroke_team_list_by_region_selected = list(stroke_team_list_by_region_selected)
-
-    with cols_regions[0]:
-        cols_t = st.columns(4)
+    # Each team input box:
+    with container_input_teams:
+        cols_t = st.columns(3)
         with cols_t[0]:
             st.markdown('All years:')
         with cols_t[1]:
@@ -411,55 +438,84 @@ def main():
                 # for vertical alignment with the other columns
                 # that have text in.
                 st.markdown('\U0000200B')
-        team_input_containers = [
-            cols_t[0], cols_t[1], cols_t[2], cols_t[3],
-            cols_t[1], cols_t[2], cols_t[3]
+        containers_list_team_inputs = [
+            cols_t[0],             # All years
+            cols_t[1], cols_t[2],  # 2016, 2017
+            cols_t[1], cols_t[2],  # 2018, 2019
+            cols_t[1], cols_t[2]   # 2020, 2021
         ]
-        stroke_teams_selected = input_stroke_teams_to_highlight(
-            stroke_team_list_by_region_selected,
-            year_options,
-            region_team_list,
-            all_years_str=all_years_str,
-            containers=team_input_containers
-            )
 
+    with container_input_regions:
+        containers_list_region_inputs = st.columns(3)
+
+    # ###########################
+    # ########## SETUP ##########
+    # ###########################
+
+    all_teams_str = 'all E+W'
+
+    # Decide which descriptive stats file to use:
+    with container_input_4hr_toggle:
         limit_to_4hr = st.toggle('Limit to arrival within 4hr')
-
     if limit_to_4hr:
         summary_stats_file = 'summary_stats_4hr.csv'
     else:
         summary_stats_file = 'summary_stats.csv'
-
+    # Read in the data:
     summary_stats_df = pd.read_csv(
-        dir + './data_descriptive/' + summary_stats_file, index_col=0
-    )
+        f'{dir}/data_descriptive/{summary_stats_file}',
+        index_col=0
+        )
 
-    # Find which teams are in the stroke teams options but
-    # are not in the stats dataframe:
-    missing_teams_list = (
-        set(stroke_team_list_years) -
-        set(summary_stats_df.columns)
-    )
+    # Import list of all stroke teams:
+    df_stroke_team = pd.read_csv(
+        f'{dir}/data_descriptive/hospitals_and_lsoas_descriptive_stats.csv',
+        index_col=False
+        ).sort_values('Stroke Team')
 
-    teams_to_show = stroke_teams_selected
+    with container_map:
+        # Plot the team locations
+        plot_geography_pins(df_stroke_team)
+
+    # List of years in the data:
+    year_options = sorted(set(summary_stats_df.loc['year']))
+    # Move the "all years" option to the front of the list:
+    all_years_str = '2016 to 2021'
+    year_options.remove(all_years_str)
+    year_options = [all_years_str] + year_options
+
+    with container_input_regions:
+        # Select regions:
+        st.markdown('Show teams from these regions:')
+        regions_selected = inputs_region_choice(
+            df_stroke_team,
+            containers_list_region_inputs
+            )
+
+    with container_input_teams:
+        # Select stroke teams:
+        stroke_teams_selected = input_stroke_teams_to_highlight(
+            df_stroke_team,
+            regions_selected,
+            all_teams_str,
+            year_options,
+            all_years_str=all_years_str,
+            containers=containers_list_team_inputs
+            )
 
     # ###########################
     # ######### RESULTS #########
     # ###########################
-    st.header('Results')
 
-    try:
-        df_to_show = summary_stats_df[teams_to_show]
-    except KeyError:
-        # Remove teams that aren't in the dataframe:
-        reduced_teams_to_show = []
-        for team in teams_to_show:
-            if team in missing_teams_list:
-                st.markdown(f':warning: There is no data for {team}.')
-            else:
-                reduced_teams_to_show.append(team)
-        df_to_show = summary_stats_df[reduced_teams_to_show]
+    # Check that all of the requested data exists.
+    # Remove any teams that don't exist and print a warning message.
+    df_to_show = check_teams_in_stats_df(
+        summary_stats_df,
+        stroke_teams_selected,
+        container_warnings
+    )
 
+    # Update the order of the rows to this:
     row_order = [
         'count',
         'age',
@@ -484,29 +540,36 @@ def main():
         'mrs 0-2'
     ]
     df_to_show = df_to_show.loc[row_order]
-    st.table(df_to_show)
+
+    with container_results_table:
+        st.header('Results')
+        st.table(df_to_show)
 
     # #########################
     # ######### PLOTS #########
     # #########################
-    feature = st.selectbox(
-        'Pick a feature to plot',
-        options=row_order,
-        # default='count'
-    )
 
-    # Remove the (year) string from the selected teams:
-    stroke_teams_selected_without_year = [
-        team.split(' (')[0] for team in stroke_teams_selected]
+    with container_violins:
+        st.header('Feature breakdown')
 
-    plot_violins(
-        summary_stats_df,
-        feature,
-        year_options,
-        stroke_teams_selected_without_year,
-        all_years_str,
-        all_teams_str
+        feature = st.selectbox(
+            'Pick a feature to plot',
+            options=row_order,
+            # default='count'
         )
+
+        # Remove the (year) string from the selected teams:
+        stroke_teams_selected_without_year = [
+            team.split(' (')[0] for team in stroke_teams_selected]
+
+        plot_violins(
+            summary_stats_df,
+            feature,
+            year_options,
+            stroke_teams_selected_without_year,
+            all_years_str,
+            all_teams_str
+            )
 
     # ----- The end! -----
 
